3c91669eb54ea1484fe2ff3c7e9375b2d2b21e69
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-02 18:19:49 +0200

    feat: bluBanner completed

--- src/components/AppBluBanner.vue
+++ src/components/AppBluBanner.vue
@@ -1,18 +1,86 @@
 <template>
     <section class="blu-banner">
-
+        <div class="container">
+            <ul>
+                <li v-for="element in bannerList">
+                    <a href="">
+                        <img :src="getImagePath(element.image)" alt="">
+                        <span>{{ element.name.toUpperCase() }}</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            bannerList: [
+                {
+                    image: "buy-comics-digital-comics.png",
+                    name: "digital comics"
+                },
+                {
+                    image: "buy-comics-merchandise.png",
+                    name: "dc merchandise"
+                },
+                {
+                    image: "buy-comics-subscriptions.png",
+                    name: "subscription"
+                },
+                {
+                    image: "buy-comics-shop-locator.png",
+                    name: "comic shop locator"
+                },
+                {
+                    image: "buy-dc-power-visa.svg",
+                    name: "dc power visa"
+                },
+                
+            ]
+        }
 
+    },
+    methods: {
+        getImagePath(img) {
+            return new URL(`../assets/img/${img}`, import.meta.url).href;
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@use "../style/_mixin.scss" as *;
+
+.container {
+    padding: 0px 10px;
+    @include flex(center, center);
+
+    ul {
+        @include flex(space-between, center);
+        color: white;
+        width: 100%;
+
+
+        li a{
+            @include flex(space-between, center);
+            gap: 10px;
+            font-size: 0.8rem;
+            text-wrap: nowrap;
+        }
+
+        img {
+            max-height: 40px;
+            max-width: 40px;
+        }
+    }
+}
+
 .blu-banner {
     height: 100px;
     background-color: blue;
+    @include flex(center, center);
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
-
+        <div class="container">
+            <h1>--> Content goes here <--</h1>
+        </div>
     </main>
 </template>
 
@@ -11,9 +13,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use "../style/_mixin.scss" as *;
 
 main {
     height: 100px;
     background-color: black;
+    @include flex(start, center);
+
+    h1 {
+        color: white;
+    }
 }
 </style>
